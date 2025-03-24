2deddba31cc055f8a5015ec94139cc40ec2d32fc
Ssempijja Charles <ssempijacharz@gmail.com>
2025-03-25 00:30:08 +0300

    Fixing the github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -21,5 +20,8 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // Ensure this matches the path in the workflow
   },
 }));
