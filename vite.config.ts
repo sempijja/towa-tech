4b457c010ee0c4300c568446e44edf745b3357f5
Ssempijja Charles <ssempijacharz@gmail.com>
2025-03-26 11:02:34 +0300

    provisional dev build

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // Use the correct base path for GitHub Pages deployment
-  base: mode === 'production' ? `/${process.env.VITE_BASE_PATH || ''}` : '/towa-tech/', 
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: mode === 'production' ? '/towa-tech/' : '/',
+    server: {
+        host: "::",
+        port: 8080,
     },
-  },
-  build: {
-    outDir: "dist", // Ensure this matches the path in the workflow
-  },
+    plugins: [react()],
+    resolve: {
+        alias: {
+        "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    build: {
+        outDir: "dist",
+    },
 }));
