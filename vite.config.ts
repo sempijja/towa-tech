96ba9307947823b38873a7441a7af7845375e56e
Ssempijja Charles <ssempijacharz@gmail.com>
2025-03-30 22:09:35 +0300

    Update favicon paths for production and adjust Vite base path for local development

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-    base: "/towa-tech/", // Set the base path to the name of the repo
+    base: mode === "production" ? "/towa-tech/" : "/", // Use "/" for local development
     server: {
         host: "::",
         port: 8080,
